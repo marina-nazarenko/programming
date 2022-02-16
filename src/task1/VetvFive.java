1bc70727053ce096f87e960a59306b5079ca8f36
DenisTumel <d.tumel@syberry.com>
2022-02-16 22:12:52 +0300

    add VetvFife

---
+++ src/task1/VetvFive.java
@@ -0,0 +1,30 @@
+package task1;
+
+import java.util.Scanner;
+
+public class VetvFive {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        VetvFive vetvFive = new VetvFive();
+        System.out.println("Введите x:");
+        double x = scanner.nextInt();
+
+        double result = vetvFive.start(x);
+        System.out.println(" F(x) = " + result);
+    }
+
+    public double start(double x) {
+        double f;
+
+        if (x <= 3) {
+            f = Math.pow(x, 2) - 3 * x + 9;
+        } else {
+            f = 1 / (Math.pow(x, 3) + 6);
+        }
+        return f;
+
+    }
+
+}
