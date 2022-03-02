643c3918931fc86c72d2f551b2b8fe16761d274f
DenisTumel <d.tumel@syberry.com>
2022-03-02 20:53:59 +0300

    add new task, Alg1

---
+++ src/task1/Alg1.java
@@ -0,0 +1,31 @@
+package task1;
+
+import java.util.Scanner;
+
+
+public class Alg1 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Alg1 alg1 = new Alg1();
+
+        System.out.println("Введите K:");
+        int k = scanner.nextInt();
+
+        alg1.arrayA(k);
+    }
+
+    private void arrayA(int k) {
+        int s = 0;
+        int[] a = new int[]{5, 25, 43, 25, 14, 35, 5, 12, 34};
+        for (int i = a[0]; i < a.length; i++) {
+            if (a[i] % k == 0) {
+                s += a[i];
+            }
+        }
+        System.out.println("Сумма, кратная числу " + k + " = " + s);
+    }
+}
+
+
