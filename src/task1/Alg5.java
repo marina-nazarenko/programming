1e8f1aa411955e183349adb5d8e51996e2448247
Mars <mars1067@mail.ru>
2022-03-05 14:15:53 +0300

    add new task, alg5

---
+++ src/task1/Alg5.java
@@ -0,0 +1,21 @@
+package task1;
+
+    public class Alg5 {
+        public static void main(String[] args) {
+            Alg5 alg5 = new Alg5();
+            alg5.replacement();
+
+        }
+
+        private void replacement() {
+            int[] a = new int[]{0, 1, 1, 4, 5, 2, 7, 11, 9, 10};
+
+            for (int i = 1; i < a.length; i++) {
+                if (a[i] > i) {
+                    System.out.print(a[i] + " ");
+                }
+            }
+        }
+    }
+
+
