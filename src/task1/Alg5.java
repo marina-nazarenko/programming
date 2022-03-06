d74b52dcdcf0a060a2be7e6b8c2214190f953108
Mars <mars1067@mail.ru>
2022-03-06 12:54:15 +0300

    fixed PR comments

--- src/task1/Alg5.java
+++ src/task1/Alg5.java
@@ -3,11 +3,10 @@ package task1;
     public class Alg5 {
         public static void main(String[] args) {
             Alg5 alg5 = new Alg5();
-            alg5.replacement();
-
+            alg5.choice();
         }
 
-        private void replacement() {
+        private void choice() {
             int[] a = new int[]{0, 1, 1, 4, 5, 2, 7, 11, 9, 10};
 
             for (int i = 1; i < a.length; i++) {
