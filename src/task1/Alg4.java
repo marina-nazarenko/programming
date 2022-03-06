03a011a58166ec86716713773927b1b87d0656b8
Mars <mars1067@mail.ru>
2022-03-06 12:46:14 +0300

    fixed

--- src/task1/Alg4.java
+++ src/task1/Alg4.java
@@ -5,35 +5,30 @@ import java.util.Arrays;
 public class Alg4 {
     public static void main(String[] args) {
         Alg4 alg4 = new Alg4();
-        alg4.replacement();
+        alg4.minMaxReplacement();
 
     }
-
-    private void replacement() {
+    private void minMaxReplacement() {
         int[] a = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(a));
         int max = a[0];
-        int imax = 0;
+        int iMax = 0;
         int min = a[0];
-        int imin = 0;
+        int iMin = 0;
         for (int i = 1; i < a.length; i++) {
             if (a[i] > max) {
                 max = a[i];
-                imax = i;
+                iMax = i;
             }
             if (a[i] < min) {
                 min = a[i];
-                imin = i;
+                iMin = i;
             }
         }
-        a[imin] = max;
-        a[imax] = min;
+        a[iMin] = max;
+        a[iMax] = min;
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
-
     }
 }
-
-
-
-
