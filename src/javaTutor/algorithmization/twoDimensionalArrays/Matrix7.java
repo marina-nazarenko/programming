2beee7c3e12569e63e43543afd887f7741ca9a42
Mars <mars1067@mail.ru>
2022-03-20 17:15:29 +0300

    add counting to matrix

--- src/javaTutor/algorithmization/twoDimensionalArrays/Matrix7.java
+++ src/javaTutor/algorithmization/twoDimensionalArrays/Matrix7.java
@@ -1,7 +1,5 @@
 package javaTutor.algorithmization.twoDimensionalArrays;
 
-import javaTutor.utils.ArrayUtils;
-
 import java.util.Scanner;
 
 public class Matrix7 {
@@ -14,7 +12,33 @@ public class Matrix7 {
         int n = scanner.nextInt();
         double[][] matrix = matrix7.creationMatrix(n);
         System.out.println("Данная матрица: ");
-        ArrayUtils.printMatrix(matrix);
+        matrix7.printMatrix(matrix);
+        System.out.println("Количество положительных элементов: ");
+        int result = matrix7.countPositiveValues(matrix);
+        System.out.println(result);
+    }
+
+    private void printMatrix(double[][] matrix) {
+        for (double[] doubles : matrix) {
+            for (double anDoubles : doubles) {
+                System.out.printf("%.2f ", anDoubles);
+            }
+            System.out.println(" ");
+        }
+    }
+
+    private int countPositiveValues(double[][] matrix) {
+        int count = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 
     private double[][] creationMatrix(int n) {
