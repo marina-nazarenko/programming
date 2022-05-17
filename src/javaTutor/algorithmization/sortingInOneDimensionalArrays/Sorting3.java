1c8d777f61a82fcfa1701ed22b568bd16e8f6482
Mars <mars1067@mail.ru>
2022-05-17 20:46:14 +0300

    Add new task, sorting3

--- src/javaTutor/algorithmization/sortingInOneDimensionalArrays/Sorting3.java
+++ src/javaTutor/algorithmization/sortingInOneDimensionalArrays/Sorting3.java
@@ -1,5 +1,7 @@
 package javaTutor.algorithmization.sortingInOneDimensionalArrays;
 
+import javaTutor.utils.ArrayUtils;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,19 +12,29 @@ public class Sorting3 {
     public static void main(String[] args) {
         Sorting3 sorting3 = new Sorting3();
 
-        System.out.println("Введите длину первого массива:");
-        int sizeArrayOne = scanner.nextInt();
-        int[] arrayOne = sorting1.generationArray(sizeArrayOne, 1);
-
-        System.out.println("\nВведите длину второго массива:");
-        int sizeArrayTwo = scanner.nextInt();
-        int[] arrayTwo = sorting1.generationArray(sizeArrayTwo, 2);
-
-        System.out.println("\nВведите число k:");
-        int k = scanner.nextInt();
+        System.out.println("Введите длину массива:");
+        int size = scanner.nextInt();
+        int[] array = ArrayUtils.generationArray(size);
 
         System.out.println("\nРезультат:");
-        int[] concatenatedArray = sorting1.unionOfArrays(arrayOne, arrayTwo, k);
+        int[] concatenatedArray = sorting3.selectionSorting(array);
         System.out.println(Arrays.toString(concatenatedArray));
+    }
+
+    private int[] selectionSorting(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int maxValue = array[i];
+            int indexMaxValue = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] > maxValue) {
+                    indexMaxValue = j;
+                    maxValue = array[j];
+                }
+            }
+            array[indexMaxValue] = array[i];
+            array[i] = maxValue;
+        }
+
+        return array;
     }
 }
