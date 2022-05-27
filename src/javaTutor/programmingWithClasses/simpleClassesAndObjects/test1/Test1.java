e92fc78c0f3c6c3c13d99e8fb0a0ca3c2a300616
Mars <mars1067@mail.ru>
2022-05-27 12:59:10 +0300

    Add new task - test1

---
+++ src/javaTutor/programmingWithClasses/simpleClassesAndObjects/test1/Test1.java
@@ -0,0 +1,34 @@
+package javaTutor.programmingWithClasses.simpleClassesAndObjects.test1;
+
+public class Test1 {
+    private int a;
+    private int b;
+
+    public void printInfo() {
+        System.out.println("А - " + this.a + "\nВ - " + this.b);
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int sumAB() {
+        return a + b;
+    }
+
+    public int maxValue() {
+        return Math.max(a, b);
+    }
+}
