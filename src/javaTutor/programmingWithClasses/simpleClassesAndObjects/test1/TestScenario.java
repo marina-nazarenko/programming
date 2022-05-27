fa270445807ba66dd1f7a1be2916cd6b918b45a8
Mars <mars1067@mail.ru>
2022-05-27 12:59:10 +0300

    Add new task - test1

---
+++ src/javaTutor/programmingWithClasses/simpleClassesAndObjects/test1/TestScenario.java
@@ -0,0 +1,28 @@
+package javaTutor.programmingWithClasses.simpleClassesAndObjects.test1;
+
+import java.util.Scanner;
+
+public class TestScenario {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+
+        System.out.println("Введите А:");
+        int a = scanner.nextInt();
+        test1.setA(a);
+
+        System.out.println("Введите Б:");
+        int b = scanner.nextInt();
+        test1.setB(b);
+
+        test1.printInfo();
+
+        int sum = test1.sumAB();
+        System.out.println("Сумма А и Б = " + sum);
+
+        int max = test1.maxValue();
+        System.out.println("Максимальное значение - " + max);
+    }
+}
