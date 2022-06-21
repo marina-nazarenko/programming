1eb7f540ccde19d9da609b7f873f84dc5124391f
Mars <mars.yyyp@gmail.com>
2022-06-21 08:01:02 +0300

    Add new task, Test2

---
+++ src/javaTutor/programmingWithClasses/simpleClassesAndObjects/testTwo/Test2.java
@@ -0,0 +1,30 @@
+package javaTutor.programmingWithClasses.simpleClassesAndObjects.testTwo;
+
+public class Test2 {
+    private int a;
+    private int b;
+
+    public Test2() {
+    }
+
+    public Test2(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
